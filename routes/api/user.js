d64c3d73b61437f1483b2028e8217575331b5d08
unknown <tcutlip08@gmail.com>
2020-03-02 22:56:11 -0500

    npm run seed wipes users too now and commented out the seed route causew drag and drop works now

--- routes/api/user.js
+++ routes/api/user.js
@@ -1,7 +1,7 @@
 const router = require("express").Router();
 const userController = require("../../controllers/userController");
 
-router.route("/admin/seeds").get(userController.seeds);
+// router.route("/admin/seeds").get(userController.seeds);
 
 router.route("/google/:sub").get(userController.findByGoogle);
 router.route("/google").put(userController.create);
